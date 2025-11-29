47cb46ed5db5f81b5a9c9d248cefa57dd05c144a
Sushant Padmanabhi <isushant.p@gmail.com>
2025-11-29 18:10:15 +0530

    Fix: updated Public IP SKU from basic to standard

--- infra/modules/virtual-machine/main.tf
+++ infra/modules/virtual-machine/main.tf
@@ -2,7 +2,8 @@ resource "azurerm_public_ip" "pip" {
     name = "${var.vm_name}-pip"
     location = var.location
     resource_group_name = var.resource_group
-    allocation_method = "Dynamic"
+    allocation_method = "Static"
+    sku = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
