3df44a7339ba207cb8e8aa449004e6b63c77cb58
Sushant Padmanabhi <isushant.p@gmail.com>
2025-12-09 12:54:03 +0530

    restructuring the modules

--- infra/modules/resource-group/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  type = string
-  description = "Name of the Resource Group"
-}
-
-variable "location" {
-  type = string
-  description = "Azure Region for the Resource Group"
-}
--- infra/modules/virtual-machine/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vm_id" {
-  value = azurerm_linux_virtual_machine.vm.id
-}
-
-output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
-}
